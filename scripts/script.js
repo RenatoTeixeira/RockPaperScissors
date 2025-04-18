050c314b5e6b62f5caaeade3f446a3e671ba4115
Renato <24220713+RenatoTeixeira@users.noreply.github.com>
2025-04-18 11:16:35 +0100

    Finished

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,9 @@ let humanChoice;
 let resultMessage;
 let choices = ["rock", "paper", "scissors"];
 let computerChoice = choices[Math.floor(Math.random() * choices.length)];
+let humanScore = 0;
+let computerScore = 0;
+let gameStatus = "Running";
 const rock = document.querySelector("#rockBtn");
 rock.addEventListener("click", () => {
   humanChoice = "rock";
@@ -20,24 +23,36 @@ scissors.addEventListener("click", () => {
   computerChoice = choices[Math.floor(Math.random() * choices.length)];
   playGame(humanChoice, computerChoice);
 });
-
 function playGame(a, b) {
   if (a === b) {
     resultMessage = "It's a draw!!";
   } else if (a === "rock" && b === "scissors") {
     resultMessage = "It's a Win!! Rock beats Scissors";
+    humanScore++;
   } else if (a === "rock" && b === "paper") {
     resultMessage = "It's a Loss!! Paper beats Rock";
+    computerScore++;
   } else if (a === "paper" && b === "scissors") {
     resultMessage = "It's a Loss!! Scissors beats Paper";
+    computerScore++;
   } else if (a === "paper" && b === "rock") {
     resultMessage = "It's a Win!! Paper beats Rock";
+    humanScore++;
   } else if (a === "scissors" && b === "paper") {
     resultMessage = "It's a Win!! Scissors beats Paper";
+    humanScore++;
   } else if (a === "scissors" && b === "rock") {
     resultMessage = "It's a Loss!! Rock beats Scissors";
+    computerScore++;
   } else {
     resultMessage = "Invalid Input!!";
+  }
+  if (computerScore === 5 || humanScore === 5) {
+    gameStatus = "Game Over";
+    computerScore = 0;
+    humanScore = 0;
+  } else {
+    gameStatus = "Running";
   }
   const human = document.querySelector("#human");
   human.textContent = humanChoice;
@@ -45,4 +60,10 @@ function playGame(a, b) {
   computer.textContent = computerChoice;
   const result = document.querySelector("#result");
   result.textContent = resultMessage;
+  const humanScoreShow = document.querySelector("#humanScore");
+  humanScoreShow.textContent = humanScore;
+  const computerScoreShow = document.querySelector("#computerScore");
+  computerScoreShow.textContent = computerScore;
+  const gameStatusShow = document.querySelector("#gameStatus");
+  gameStatusShow.textContent = gameStatus;
 }
